cbe466129074536ee75531e1957c73a3c73e167f
Greg Ford <88810845+gregsford@users.noreply.github.com>
2023-04-11 23:11:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("April 11, 2023 23:10:00").getTime();
+    const countDate = new Date("July 1, 2023 14:00:00").getTime();
     const now = new Date().getTime();
     const timeGap = countDate - now;
 
